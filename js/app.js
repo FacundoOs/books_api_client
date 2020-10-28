5d95cf4e6cb07b9d10567e19e8c7e1f375099965
FacundoOs <osores.facundo@gmail.com>
2020-10-28 16:38:54 +0100

    Create a form in index.html for the creattion of author and title. Create a submitHandler to manage the post request

--- js/app.js
+++ js/app.js
@@ -7,14 +7,30 @@ const displayBooks = async () => {
   data.books.forEach((book) => {
     const displayBookElement = document.createElement("div");
     let html = `<p>The book ${book.title}, is written by ${book.author}. `;
-    
+
     // The innerHTML property sets or returns the HTML content (inner HTML) of an element.
     displayBookElement.innerHTML = html;
     // The appendChild() method appends a node as the last child of a node.
     displayElement.appendChild(displayBookElement);
   });
 };
+
+const submitHandler = async () => {
+  event.preventDefault();
+  let author = event.target.author.value;
+  let title = event.target.title.value;
+  const response = await fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({ title: title, author: author }),
+  });
+};
 //attaches an event handler to the document
 document.addEventListener("DOMContentLoaded", () => {
   displayBooks();
+  const createBookForm = document.getElementById("create-book");
+  createBookForm.addEventListener("submit", submitHandler);
 });
